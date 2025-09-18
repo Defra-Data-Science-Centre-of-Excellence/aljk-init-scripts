e727e865624d43563127ea49df01083a468ea1fc
EA-Ammar <ammar.ljubijankic@environment-agency.gov.uk>
2025-09-18 15:59:27 +0100

    intro more packages

--- DASH.sh
+++ DASH.sh
@@ -2,11 +2,13 @@
 set -ex
 echo $DB_CLUSTER_NAME
 
-# Optional cscrootcerts.crt path
-CERT_PATH=
+# Copy in pip.conf file
+cp /dbfs/databricks/scripts/pip.conf /etc/pip.conf
 
-URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/init-scripts/main/src
-wget $URL/rootcert.sh -O- | bash -ex
+# Optional cscrootcerts.crt path
+export CERT_PATH="/Volumes/prd_dash_config/common/init_scripts/src/cscrootcert.crt"
+
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/aljk-init-scripts/main/src
 wget $URL/update.sh -O- | bash  -ex
 wget $URL/base_libs.sh -O- | bash  -ex
 if [[ "$DB_CLUSTER_NAME" == *"RStu"* ]]; then  # If RStu in cluster name
--- src/r_installs.R
+++ src/r_installs.R
@@ -77,3 +77,39 @@ install.packages(c(
     "gllvm",
     "brickster"
 ), repos="https://cloud.r-project.org")
+
+install.packages("here") # To set project folder dynamically
+install.packages("remotes") # Makes it possible to load github package where necesary
+install.packages("knitr") # Needed to knit markdown
+install.packages("tidyverse") # Whole tidyverse package
+install.packages("lubridate") # Tideyverse date manipulations
+install.packages("scales") # For scales in ggplots
+install.packages("ggplot2") # Used for making main charts
+install.packages("nomisr") # Downloading data from Nomis
+install.packages("devtools") # Allows downloading ggla packages
+install.packages("data.table") # Data table utility
+install.packages("janitor") # Cleaning up date files
+install.packages("ggrepel") # Used to repel overlapping text in charts [used in gglaplot?]
+install.packages("plotly") # Interactive charts
+install.packages("leaflet") # Interactive maps
+install.packages("leafsync")# Syncing two maps next to each other
+#install.packages("rgdal") # Needed for geospatial manipulations
+install.packages("httr") # Quering websites to check if data exists
+install.packages("flextable") # Summary headline stats table
+install.packages("officer") # for fp_text
+install.packages("extrafont") #Fonts for the flextable
+install.packages("svDialogs") #for pop-ups
+install.packages("sf") # for geometry
+install.packages("reactable") #For interactive tables
+install.packages("openxlsx") #For exporting
+install.packages("svglite") # this should not be necessary
+install.packages("labelled") # for labels on variables
+install.packages("RColorBrewer") # for color palettes
+devtools::install_github("ammar-gla/gglaplot") # GLA plotting functions - TO BE CHANGED INTO EA
+install.packages("shiny") # to create shiny applications
+install.packages("shinydashboard") # easy dashboards
+install.packages("rsconnect") # To deploy shiny
+install.packages("fresh") # change the theme colors of Shinydashboard
+
+# install.packages("remotes")
+remotes::install_github("TimTeaFan/dplyover")
